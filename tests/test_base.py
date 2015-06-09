bcfc8bd986370cd77d9f8ffb9b680e8a8baaa565
OctavianLee <octavianlee1@gmail.com>
2015-06-09 21:56:07 +0800

    Adds the mock and updates some tests of the basic class.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,8 +1,9 @@
 #-*- coding: utf-8 -*-
 import json
+import mock
 import unittest
+from nose.tools import eq_, ok_
 from .constants import CONST
-from .conf.config import appid, appsecret
 
 from pywechat import WechatService
 from pywechat.excepts import WechatError
@@ -13,16 +14,18 @@ class TestCase(unittest.TestCase):
     '''Creates a TestCase template to help test.'''
 
     def setUp(self):
-        self.service = WechatService(appid, appsecret)
+        self.service = WechatService(CONST.STRING, CONST.STRING)
 
 
 class BasicTest(unittest.TestCase):
     '''Creates a TestCase for the class of Basic.'''
 
     def setUp(self):
-        self.basic = Basic(appid, appsecret)
+        with mock.patch.object(Basic, 'access_token', autospec=True):
+            self.basic = Basic(CONST.STRING, CONST.STRING)
 
     def test_check_wechat_error(self):
+        '''Tests the _check_wechat_error method.'''
         errcode = CONST.NUMBER
         errmsg = CONST.STRING
         data = {
@@ -32,7 +35,36 @@ class BasicTest(unittest.TestCase):
         with self.assertRaises(WechatError) as e:
             self.basic._check_wechat_error(data)
         exception = e.exception
-        self.assertEqual(exception.code, errcode)
-        self.assertEqual(exception.message, errmsg)
+        eq_(exception.code, errcode)
+        eq_(exception.message, errmsg)
 
+    def test_send_request(self):
+        '''Tests the _send_request  method.'''
+        with mock.patch.object(Basic, '_send_request'):
+            ok_(self.basic._send_request('get', CONST.STRING))
+            ok_(self.basic._send_request('post', CONST.STRING))
 
+    def test_grant_access_token(self):
+        '''Tests the _grant_access_token method.'''
+        with mock.patch.object(Basic, '_send_request') as mock_method:
+            access_token = CONST.STRING
+            expires_in = CONST.NUMBER
+            mock_method.return_value = {
+              "access_token": access_token,
+              "expires_in": expires_in
+            }
+            data = self.basic._grant_access_token()
+            eq_(data.get('access_token'), access_token)
+            eq_(data.get('expires_in'), expires_in)
+
+    def test_get_wechat_server_ips(self):
+        '''Tests the _get_wechat_server_ips method.'''
+        with mock.patch.object(Basic, '_send_request') as mock_method:
+            with mock.patch.object(Basic, '_grant_access_token') as method:
+                ips = [CONST.STRING] * CONST.NUMBER
+                method.return_value = CONST.STRING
+                mock_method.return_value = {
+                  "ips": ips
+                }
+                data = self.basic._get_wechat_server_ips()
+                eq_(data.get('ips'), ips)
