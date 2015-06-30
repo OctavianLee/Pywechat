98aa85a32ff05a80ca18855216dcd3320319759c
OctavianLee <octavianlee1@gmail.com>
2015-06-30 22:46:25 +0800

    Finishes the tests of shakearound.

--- tests/test_service/test_shakearound.py
+++ tests/test_service/test_shakearound.py
@@ -191,3 +191,206 @@ class ShakeServiceTest(TestCase):
             data = self.shakeservice.add_page(title, description, page_url,
                                               icon_url, comment)
             eq_(data["data"]["page_id"], page_id)
+
+    def test_update_page(self):
+        title = CONST.STRING
+        description = CONST.STRING
+        page_url = CONST.STRING
+        icon_url = CONST.STRING
+        comment = CONST.STRING
+        page_id = CONST.NUMBER
+        with mock.patch.object(ShakeService, 'update_page') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                    "page_id": page_id
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.update_page(page_id, title, description, page_url,
+                                                 icon_url, comment)
+            eq_(data["data"]["page_id"], page_id)
+
+    def test_search_page_by_ids(self):
+        title = CONST.STRING
+        description = CONST.STRING
+        page_url = CONST.STRING
+        icon_url = CONST.STRING
+        comment = CONST.STRING
+        page_id = CONST.NUMBER
+        page_ids = [page_id]
+        with mock.patch.object(ShakeService, 'search_page_by_ids') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                    "pages": [
+                        {
+                            "comment": comment,
+                            "description": description,
+                            "icon_url": icon_url,
+                            "page_id": page_id,
+                            "page_url": page_url,
+                            "title": title
+                        }
+                    ],
+                    "total_count": 1
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.search_page_by_ids(page_ids)
+            page_data = {
+                "comment": comment,
+                "description": description,
+                "icon_url": icon_url,
+                "page_id": page_id,
+                "page_url": page_url,
+                "title": title
+            }
+            eq_(data["data"]["total_count"], 1)
+            eq_(data["data"]["pages"][0], page_data)
+
+    def test_search_page_by_counts(self):
+        title = CONST.STRING
+        description = CONST.STRING
+        page_url = CONST.STRING
+        icon_url = CONST.STRING
+        comment = CONST.STRING
+        page_id = CONST.NUMBER
+        with mock.patch.object(ShakeService, 'search_page_by_counts') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                    "pages": [
+                        {
+                            "comment": comment,
+                            "description": description,
+                            "icon_url": icon_url,
+                            "page_id": page_id,
+                            "page_url": page_url,
+                            "title": title
+                        }
+                    ],
+                    "total_count": 1
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.search_page_by_counts(0, 1)
+            page_data = {
+                "comment": comment,
+                "description": description,
+                "icon_url": icon_url,
+                "page_id": page_id,
+                "page_url": page_url,
+                "title": title
+            }
+            eq_(data["data"]["total_count"], 1)
+            eq_(data["data"]["pages"][0], page_data)
+
+    def test_delete_page(self):
+        page_id = CONST.NUMBER
+        page_ids = [page_id]
+        with mock.patch.object(ShakeService, 'delete_page') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.delete_page(page_ids)
+            self.assertIsNotNone(data)
+
+    def test_bind_page(self):
+        page_id = CONST.NUMBER
+        page_ids = [page_id]
+        bind = 0
+        append = 0
+        device_id = CONST.NUMBER
+        uuid = CONST.STRING
+        major = CONST.STRING
+        minor = CONST.STRING
+        with mock.patch.object(ShakeService, 'bind_location') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.bind_location(page_ids)
+            self.assertIsNotNone(data)
+
+    def test_get_shake_info(self):
+        ticket = CONST.NUMBER
+        need_poi = 1
+        with mock.patch.object(ShakeService, 'get_shake_info') as mock_method:
+            mock_method.return_value = {
+                "data": {
+                },
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.get_shake_info(ticket, need_poi)
+            self.assertIsNotNone(data)
+
+    def test_device_statistics(self):
+        begin_date = CONST.STRING
+        end_date = CONST.STRING
+        device_id = CONST.NUMBER
+        uuid = CONST.STRING
+        major = CONST.STRING
+        minor = CONST.STRING
+        with mock.patch.object(ShakeService, 'device_statistics') as mock_method:
+            ftime = CONST.NUMBER
+            mock_method.return_value = {
+                "data": [
+                    {
+                        "click_pv": 0,
+                        "click_uv": 0,
+                        "ftime": ftime,
+                        "shake_pv": 0,
+                        "shake_uv": 0
+                    }
+                ],
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.device_statistics(begin_date, end_date,
+                                                       device_id, uuid, major, minor)
+            statistics = {
+                "click_pv": 0,
+                "click_uv": 0,
+                "ftime": ftime,
+                "shake_pv": 0,
+                "shake_uv": 0
+
+            }
+            eq_(data["data"][0], statistics)
+
+    def test_page_statistics(self):
+        begin_date = CONST.STRING
+        end_date = CONST.STRING
+        page_id = CONST.NUMBER
+        with mock.patch.object(ShakeService, 'page_statistics') as mock_method:
+            ftime = CONST.NUMBER
+            mock_method.return_value = {
+                "data": [
+                    {
+                        "click_pv": 0,
+                        "click_uv": 0,
+                        "ftime": ftime,
+                        "shake_pv": 0,
+                        "shake_uv": 0
+                    }
+                ],
+                "errcode": 0,
+                "errmsg": "success."
+            }
+            data = self.shakeservice.page_statistics(page_id, begin_date,
+                                                     end_date)
+            statistics = {
+                "click_pv": 0,
+                "click_uv": 0,
+                "ftime": ftime,
+                "shake_pv": 0,
+                "shake_uv": 0
+            }
+            eq_(data["data"][0], statistics)
