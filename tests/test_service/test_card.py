d9ceae01c5da0c8341fa7b5a964f6e1f7a3a9c35
OctavianLee <octavianlee1@gmail.com>
2015-07-01 21:21:25 +0800

    Finishes most of tests in the card management.

--- tests/test_service/test_card.py
+++ tests/test_service/test_card.py
@@ -41,3 +41,123 @@ class CardServiceTest(TestCase):
             }
             data = self.card_service.get_colors()
             self.assertIsNotNone(data["colors"])
+
+    def test_upload_image(self):
+        image = CONST.STRING
+        pic_url = CONST.STRING
+        with mock.patch.object(CardService, 'upload_image') as mock_method:
+            mock_method.return_value = {
+                "url": pic_url
+            }
+            data = self.card_service.upload_image(image)
+            eq_(data["url"], pic_url)
+
+    def test_unavilable_code(self):
+        code = CONST.STRING
+        card_id = CONST.STRING
+        openid = CONST.STRING
+        with mock.patch.object(CardService, 'unavailable_code') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+                "card": {"card_id": card_id},
+                "openid": openid
+
+            }
+            data = self.card_service.unavailable_code(code, card_id)
+            eq_(data["card"]["card_id"], card_id)
+            eq_(data["openid"], openid)
+
+    def test_decrypt_code(self):
+        encrypt_code = CONST.STRING
+        card_id = CONST.STRING
+        openid = CONST.STRING
+        with mock.patch.object(CardService, 'decrypt_code') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+                "card": {"card_id": card_id},
+                "openid": openid
+
+            }
+            data = self.card_service.decrypt_code(encrypt_code)
+            eq_(data["card"]["card_id"], card_id)
+            eq_(data["openid"], openid)
+
+    def test_get_code(self):
+        code = CONST.STRING
+        card_id = CONST.STRING
+        begin_time = CONST.NUMBER
+        end_time = CONST.NUMBER
+        openid = CONST.STRING
+        with mock.patch.object(CardService, 'get_code') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+                "card": {
+                    "card_id": card_id,
+                    "begin_time": begin_time,
+                    "end_time": end_time,
+                },
+                "openid": openid
+
+            }
+            card_data = {
+                "card_id": card_id,
+                "begin_time": begin_time,
+                "end_time": end_time,
+            }
+            data = self.card_service.get_code(code, card_id)
+            eq_(data["card"], card_data)
+            eq_(data["openid"], openid)
+
+    def test_batchget_code(self):
+        card_id = CONST.STRING
+        with mock.patch.object(CardService, 'batchget_card') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+                "card_id_list": [card_id],
+                "total_num": 1
+            }
+            data = self.card_service.batchget_card(0, 1)
+            eq_(data["card_id_list"][0], card_id)
+            eq_(data["total_num"], 1)
+
+    def test_modify_stock(self):
+        card_id = CONST.STRING
+        increase_stock_value = CONST.NUMBER
+        reduce_stock_value = CONST.NUMBER
+
+        with mock.patch.object(CardService, 'modify_stock') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+            }
+            data = self.card_service.modify_stock(card_id,
+                                                  increase_stock_value, reduce_stock_value)
+            self.assertIsNotNone(data)
+
+    def test_update_code(self):
+        code = CONST.STRING
+        new_code = CONST.STRING
+        card_id = CONST.STRING
+
+        with mock.patch.object(CardService, 'update_code') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+            }
+            data = self.card_service.update_code(code, new_code, card_id)
+            self.assertIsNotNone(data)
+
+    def test_delete_card(self):
+        card_id = CONST.STRING
+
+        with mock.patch.object(CardService, 'delete_card') as mock_method:
+            mock_method.return_value = {
+                "errcode": 0,
+                "errmsg": "ok",
+            }
+            data = self.card_service.delete_card(card_id)
+            self.assertIsNotNone(data)
